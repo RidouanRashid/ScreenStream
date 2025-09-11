802380c4f997736b9df86da83ff8da4972824ac3
JelleRomijn <jelletijmen@gmail.com>
2025-09-11 13:05:37 +0200

    nav en hero
    nav en hero banner gemaakt

---
+++ includes/hero.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="hero-section">
+        <div class="hero-overlay"></div>
+        <div class="hero-content">
+            <h1>WELKOM BIJ ANNEXBIOS 3</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient</p>
+            <a href="#" class="hero-btn">BEKIJK DE DRAAIENDE FILMS</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title> 
+     <link rel="stylesheet" href="css/style.css?v="> 
+</head>
+<body>
+    <nav class="navbar">
+        <div class="navbar-logo">
+            <a href="#">mooi logo</a>
+        </div>
+        <ul>
+            <li><a href="filmagenda.php">FILM AGENDA</a></li>
+            <li><a href="#">ALLE VESTIGINGEN</a></li>
+            <li><a href="contact.php">CONTACT</a></li>
+        </ul>
+    </nav>
+    <div class="header-bar">
+        <span class="tickets-title">KOOP JE TICKETS</span>
+        <select class="tickets-select">
+            <option>Kies je film</option>
+            <option>Film 1</option>
+            <option>Film 2</option>
+        </select>
+        <button class="tickets-btn">BESTEL TICKETS</button>
+    </div>
+</body>
+</html>
