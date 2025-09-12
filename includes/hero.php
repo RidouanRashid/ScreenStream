7f461791557caf404f71f2880054c5d2af0b8a8f
JelleRomijn <jelletijmen@gmail.com>
2025-09-12 11:23:16 +0200

    hero en navbar

---
+++ includes/hero.php
@@ -0,0 +1,8 @@
+<div class="hero-section">
+    <div class="hero-overlay"></div>
+    <div class="hero-content">
+        <h1>WELKOM BIJ ANNEXBIOS 3</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient</p>
+        <a href="#" class="hero-btn">BEKIJK DE DRAAIENDE FILMS</a>
+    </div>
+</div>
---
+++ includes/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar">
+    <div class="navbar-logo">
+        <a href="#">mooi logo</a>
+    </div>
+    <ul>
+        <li><a href="filmagenda.php">FILM AGENDA</a></li>
+        <li><a href="#">ALLE VESTIGINGEN</a></li>
+        <li><a href="contact.php">CONTACT</a></li>
+    </ul>
+</nav>
+<div class="header-bar">
+    <span class="tickets-title">KOOP JE TICKETS</span>
+    <select class="tickets-select">
+        <option>Kies je film</option>
+        <option>Film 1</option>
+        <option>Film 2</option>
+    </select>
+    <button class="tickets-btn">BESTEL TICKETS</button>
+</div>
