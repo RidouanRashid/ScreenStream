568c6c1172a6abfae3b450b2749e7b9523370490
JelleRomijn <jelletijmen@gmail.com>
2025-09-17 13:49:47 +0200

    filter

---
+++ includes/filmfilter.php
@@ -0,0 +1,13 @@
+<div class="filmagenda-filter-section">
+    <h2 class="filmagenda-title">FILM AGENDA</h2>
+    <div class="filmagenda-filter-row">
+       
+        <button class="filmagenda-filter-btn active">FILMS</button>
+        <button class="filmagenda-filter-btn">DEZE WEEK</button>
+        <button class="filmagenda-filter-btn">VANDAAG</button>
+        <div class="filmagenda-filter-dropdown">
+            <button class="filmagenda-filter-btn">CATEGORIE <span class="dropdown-arrow">&#9662;</span></button>
+            <!-- dropdown menu kan hier -->
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -3,5 +3,6 @@ include 'includes/header.php';
 include 'includes/navbar.php';
 include 'includes/hero.php';
 include 'includes/googlemaps.php';
+include 'includes/filmfilter.php';
 include 'includes/footer.php';
 ?>
