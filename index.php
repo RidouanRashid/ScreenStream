329532ce4267a0f855c9ed2de77d7f40a88af804
MLijffijt <mg_lijffijt@ziggo.nl>
2025-09-18 10:39:51 +0200

    hihiihihih

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
+include 'includes/header.php';
 include 'includes/navbar.php';
-include 'includes/film-agenda.php';
 include 'includes/hero.php';
+include 'includes/googlemaps.php';
+include 'includes/filmfilter.php';
+include 'includes/film-agenda.php';
+include 'includes/footer.php';
 ?>
